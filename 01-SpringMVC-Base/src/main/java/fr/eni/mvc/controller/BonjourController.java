bd6d4c741608d12bf4622312430c698d0ae2c29f
Marc <harnist.marc@gmail.com>
2019-08-29 11:48:46 +0200

    creation

---
+++ 01-SpringMVC-Base/src/main/java/fr/eni/mvc/controller/BonjourController.java
@@ -0,0 +1,21 @@
+package fr.eni.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(path="/bonjour")
+public class BonjourController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String bonjour(){
+		return "bonjour";
+	}
+
+	@RequestMapping(method=RequestMethod.GET, path="/hello")
+	public String hello(){
+		return "hello";
+	}
+}
+
